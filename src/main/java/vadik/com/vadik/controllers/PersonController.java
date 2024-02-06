2edd55267ad12eec1f113633cdda7b2a01bc5455
Tony <makotra.anton@prachka.space>
2024-02-06 16:44:49 +0300

    psql commented for docker

--- src/main/java/vadik/com/vadik/controllers/PersonController.java
+++ src/main/java/vadik/com/vadik/controllers/PersonController.java
@@ -4,14 +4,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import vadik.com.vadik.entity.PersonEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import vadik.com.vadik.model.PersonModel;
 import vadik.com.vadik.service.PersonService;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @Slf4j
 @RestController
@@ -25,59 +23,59 @@ public class PersonController {
                 .build();
     }
 
-    @GetMapping("/byage")
-    public ResponseEntity<List<PersonModel>> getByAge(@RequestParam Integer age){
-        return ResponseEntity.ok()
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(personService.findByAgeGreaterThanValue(age));
-    }
+//    @GetMapping("/byage")
+//    public ResponseEntity<List<PersonModel>> getByAge(@RequestParam Integer age){
+//        return ResponseEntity.ok()
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .body(personService.findByAgeGreaterThanValue(age));
+//    }
 
     @GetMapping("/byname")
     public ResponseEntity<String> getByName(@RequestParam String name){
-        PersonEntity body = personService.findByName(name);
-        log.info(body.toString());
+        String body = personService.findByName(name);
+        log.info(body);
         return ResponseEntity.ok()
                 .contentType(MediaType.APPLICATION_JSON)
-                .body(body.toString());
+                .body(body);
     }
 
-    @PostMapping("/persons")
-    public PersonEntity createPerson(@RequestBody PersonModel personModel) {
-        PersonEntity personEntity = new PersonEntity();
-        personEntity.setAge(personModel.getAge());
-        personEntity.setName(personModel.getName());
-        personEntity.setEmail(personModel.getEmail());
-        return personService.addPerson(personEntity);
-    }
+//    @PostMapping("/persons")
+//    public PersonEntity createPerson(@RequestBody PersonModel personModel) {
+//        PersonEntity personEntity = new PersonEntity();
+//        personEntity.setAge(personModel.getAge());
+//        personEntity.setName(personModel.getName());
+//        personEntity.setEmail(personModel.getEmail());
+//        return personService.addPerson(personEntity);
+//    }
 
     // get employee by id rest api
-    @GetMapping("/persons/{id}")
-    public ResponseEntity<PersonEntity> getPersonById(@PathVariable Integer id) {
-        PersonEntity employee = personService.findById(id);
-        return ResponseEntity.ok(employee);
-    }
+//    @GetMapping("/persons/{id}")
+//    public ResponseEntity<PersonEntity> getPersonById(@PathVariable Integer id) {
+//        PersonEntity employee = personService.findById(id);
+//        return ResponseEntity.ok(employee);
+//    }
 
     // update employee rest api
 
-    @PutMapping("/persons/{id}")
-    public ResponseEntity<PersonModel> updateEmployee(@PathVariable Integer id, @RequestBody PersonModel personModel){
-        PersonEntity personEntity = personService.findById(id);
-        personEntity.setAge(personModel.getAge());
-        personEntity.setEmail(personModel.getEmail());
-        personEntity.setName(personModel.getName());
-        personService.save(personEntity);
-
-        return ResponseEntity.ok(personModel);
-    }
+//    @PutMapping("/persons/{id}")
+//    public ResponseEntity<PersonModel> updateEmployee(@PathVariable Integer id, @RequestBody PersonModel personModel){
+//        PersonEntity personEntity = personService.findById(id);
+//        personEntity.setAge(personModel.getAge());
+//        personEntity.setEmail(personModel.getEmail());
+//        personEntity.setName(personModel.getName());
+//        personService.save(personEntity);
+//
+//        return ResponseEntity.ok(personModel);
+//    }
 
     // delete employee rest api
-    @DeleteMapping("/persons/{id}")
-    public ResponseEntity<Map<String, Boolean>> deleteEmployee(@PathVariable Integer id){
-        PersonEntity personEntity = personService.findById(id);
-
-        personService.delete(personEntity);
-        Map<String, Boolean> response = new HashMap<>();
-        response.put("deleted", Boolean.TRUE);
-        return ResponseEntity.ok(response);
-    }
+//    @DeleteMapping("/persons/{id}")
+//    public ResponseEntity<Map<String, Boolean>> deleteEmployee(@PathVariable Integer id){
+//        PersonEntity personEntity = personService.findById(id);
+//
+//        personService.delete(personEntity);
+//        Map<String, Boolean> response = new HashMap<>();
+//        response.put("deleted", Boolean.TRUE);
+//        return ResponseEntity.ok(response);
+//    }
 }
--- src/main/java/vadik/com/vadik/repository/PersonRepository.java
+++ src/main/java/vadik/com/vadik/repository/PersonRepository.java
@@ -1,20 +1,20 @@
-package vadik.com.vadik.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
-import vadik.com.vadik.entity.PersonEntity;
-
-import java.util.List;
-
-@Repository
-public interface  PersonRepository extends JpaRepository<PersonEntity, Integer> {
-
-     @Query("select ps from PersonEntity ps where ps.age > :age")
-     List<PersonEntity> findByAge(@Param("age") Integer age);
-
-     PersonEntity findByName(String name);
-
-
-}
+//package vadik.com.vadik.repository;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.Query;
+//import org.springframework.data.repository.query.Param;
+//import org.springframework.stereotype.Repository;
+//import vadik.com.vadik.entity.PersonEntity;
+//
+//import java.util.List;
+//
+////@Repository
+////public interface  PersonRepository extends JpaRepository<PersonEntity, Integer> {
+////
+//////     @Query("select ps from PersonEntity ps where ps.age > :age")
+//////     List<PersonEntity> findByAge(@Param("age") Integer age);
+////
+//////     PersonEntity findByName(String name);
+////
+////
+////}
